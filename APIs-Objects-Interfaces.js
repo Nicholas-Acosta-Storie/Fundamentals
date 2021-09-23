92d2473f2b145642c433232422658dfd4bc329c4
nicho <nacostastorie@cnm.edu>
2021-09-22 18:27:30 -0600

    commit Fundamentals project

---
+++ APIs-Objects-Interfaces.js
@@ -0,0 +1,28 @@
+//APIs, Objects, and Interfaces
+/*
+let dog = {
+    name: "Duke",
+    color: "black",
+    breed: "pitBull",
+    size: "large",
+    bark: function(typeOfBark){
+        console.log("Bark!");
+
+    },
+
+};
+
+dog.bark();
+*/
+
+function x(y) {
+    y.number = y.number + 5;
+    console.log(y);
+}
+
+let y = {
+    name: "tom",
+    number: 10,
+};
+x(y);
+console.log(y);
---
+++ variables-and-data.js
@@ -0,0 +1,13 @@
+
+
+
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2*4;
+let division = 4 / 2;
+let mod =  5 % 2;
+
+let string = "I am practicing " + "On my js skills";
+
+console.log(string);
+
